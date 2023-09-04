632760f90182128e54f92f1158ee5ccce85b91c2
Subham Samanta <subham@synergicsoftek.in>
2023-09-04 19:15:08 +0530

    act material 04-09-2023

--- modules/DataCollectionModule.js
+++ modules/DataCollectionModule.js
@@ -10,5 +10,15 @@ module.exports = {
             var res_dt = await db_Select(select, table_name, whr, order)
             resolve(res_dt)
         })
+    },
+    getActMetrialDtls: (sec_id, ind_id) => {
+        return new Promise(async (resolve, reject) => {
+            var select = 'a.id, a.sec_id, b.sec_name, a.ind_id, b.sec_name, a.sl_no, a.act_metric, a.catg, a.unit, a.code',
+            table_name = 'td_act_metrics a, md_sector b, md_industries c',
+            whr = `a.sec_id=b.id AND a.ind_id=c.id AND b.id=c.sec_id ${sec_id > 0 ? `AND a.sec_id = ${sec_id}` : ''} ${ind_id > 0 ? `AND a.ind_id = ${ind_id}` : ''}`,
+            order = 'ORDER BY a.sl_no';
+            var res_dt = await db_Select(select, table_name, whr, order)
+            resolve(res_dt)
+        })
     }
 }
--- router/DataCollectionRouter.js
+++ router/DataCollectionRouter.js
@@ -3,7 +3,7 @@ const express = require("express"),
   dateFormat = require('dateformat');
 
 const { getSectorList, getIndustriesList } = require("../modules/AdminModule");
-const { getSusDiscList } = require("../modules/DataCollectionModule");
+const { getSusDiscList, getActMetrialDtls } = require("../modules/DataCollectionModule");
 const { db_Insert, db_Select } = require("../modules/MasterModule");
 
 DataCollectionRouter.get("/sus_disc", async (req, res) => {
@@ -73,5 +73,71 @@ DataCollectionRouter.get("/sus_disc_ajax", async (req, res) => {
   res_dt = {suc: res_dt.suc > 0 ? (res_dt.msg.length > 0 ? 1 : 2) : res_dt.suc,  msg: res_dt.msg}
   res.send(res_dt)
 });
+
+DataCollectionRouter.get('/act_metric', async (req, res) => {
+  var selected = {
+    sec_id: req.query.sec_id ? req.query.sec_id : 0,
+    ind_id: req.query.ind_id ? req.query.ind_id : 0,
+  };
+  var sec_data = await getSectorList(),
+    ind_list = {msg:[]};
+  if (selected.sec_id > 0)
+    ind_list = await getIndustriesList(null, selected.sec_id);
+  // console.log(ind_list);
+  var data = {
+    sec_data,
+    ind_list,
+    selected,
+    header: "Activity Metrics",
+  };
+  res.render("data_collection/act_metric", data);
+})
+
+DataCollectionRouter.post('/save_act_metric', async (req, res) => {
+  var data = req.body, res_dt, 
+  user = 'admin', 
+  datetime = dateFormat(new Date(), "yyyy-mm-dd HH:MM:ss");
+  let j = 1
+  if(Array.isArray(data.act_metric)){
+    for(let i = 0; i<data.act_metric.length; i++){
+      var chk_whr = `sec_id = '${data.sec_id}' AND ind_id = '${data.ind_id}' AND sl_no = ${j}`;
+      var chk_dt = await db_Select('id', 'td_act_metrics', chk_whr, null)
+      
+      var table_name = `td_act_metrics`, 
+      fields = chk_dt.suc > 0 && chk_dt.msg.length > 0 ? `act_metric = '${data.act_metric[i]}', catg = '${data.catg[i]}', unit = '${data.unit[i]}', code = '${data.code[i]}', modified_by= '${user}', modified_dt = '${datetime}'` : 
+        '(sec_id, ind_id, sl_no, act_metric, catg, unit, code, created_by, created_dt)', 
+      values = `('${data.sec_id}', '${data.ind_id}', '${j}', '${data.act_metric[i]}', '${data.catg[i]}', '${data.unit[i]}', '${data.code[i]}', '${user}', '${datetime}')`,
+      whr = chk_dt.suc > 0 && chk_dt.msg.length > 0 ? `id = '${chk_dt.msg[0].id}'` : null, 
+      flag = chk_dt.suc > 0 && chk_dt.msg.length > 0 ? 1 : 0;
+      res_dt = await db_Insert(table_name, fields, values, whr, flag)
+      j++
+    }
+  }else{
+    var chk_whr = `sec_id = '${data.sec_id}' AND ind_id = '${data.ind_id}' AND sl_no = ${j}`;
+    var chk_dt = await db_Select('id', 'td_act_metrics', chk_whr, null)
+    
+    var table_name = `td_act_metrics`, 
+    fields = chk_dt.suc > 0 && chk_dt.msg.length > 0 ? `act_metric = '${data.act_metric}', catg = '${data.catg}', unit = '${data.unit}', code = '${data.code}', modified_by= '${user}', modified_dt = '${datetime}'` : 
+      '(sec_id, ind_id, sl_no, act_metric, catg, unit, code, created_by, created_dt)', 
+    values = `('${data.sec_id}', '${data.ind_id}', '${j}', '${data.act_metric}', '${data.catg}', '${data.unit}', '${data.code}', '${user}', '${datetime}')`,
+    whr = chk_dt.suc > 0 && chk_dt.msg.length > 0 ? `id = '${chk_dt.msg[0].id}'` : null, 
+    flag = chk_dt.suc > 0 && chk_dt.msg.length > 0 ? 1 : 0;
+    res_dt = await db_Insert(table_name, fields, values, whr, flag)
+  }
+
+  req.session.message = {
+    type: res_dt.suc > 0 ? "success" : "danger",
+    message: res_dt.msg,
+  };
+  res.redirect(`/act_metric?sec_id=${data.sec_id}&ind_id=${data.ind_id}`);
+  // res.send(res_dt)
+})
+
+DataCollectionRouter.get('/get_act_metric_ajax', async (req, res) => {
+  var data = req.query
+  var res_dt = await getActMetrialDtls(data.sec_id, data.ind)
+  res_dt = {suc: res_dt.suc > 0 ? (res_dt.msg.length > 0 ? 1 : 2) : res_dt.suc, msg: res_dt.msg}
+  res.send(res_dt)
+})
 
 module.exports = { DataCollectionRouter };
