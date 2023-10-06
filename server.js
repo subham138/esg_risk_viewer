57f01ebbaf5259c229839bcdb360fe5e478b3c33
Subham Samanta <subham@synergicsoftek.in>
2023-10-06 16:32:21 +0530

    Save

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.static(path.join(__dirname, "assets/")));
 // END //
 
 // SET REQUEST HANDLER //
-// app.use(express.json());
+// app.use(express.json()); //
 // app.use(express.urlencoded({ extended: false }));
 app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({limit: '50mb', extended: false}));
