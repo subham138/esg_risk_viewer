af174ef275cdaa82940bf6554eb36ed50fd53115
Subham Samanta <subham@synergicsoftek.in>
2023-10-09 19:18:48 +0530

    Dynamic data entry 09-10-2023

--- db/db.js
+++ db/db.js
@@ -1,22 +1,22 @@
 const mysql = require("mysql");
 
 // LOCAL //
-const db = mysql.createPool({
-  connectionLimit: 10,
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "esg_risk_viewer",
-});
-
-// SERVER //
 // const db = mysql.createPool({
 //   connectionLimit: 10,
 //   host: "localhost",
 //   user: "root",
-//   password: "Cloud#1851",
+//   password: "",
 //   database: "esg_risk_viewer",
 // });
+
+// SERVER //
+const db = mysql.createPool({
+  connectionLimit: 10,
+  host: "localhost",
+  user: "root",
+  password: "Cloud#1851",
+  database: "esg_risk_viewer",
+});
 
 
 db.getConnection((err, connection) => {
