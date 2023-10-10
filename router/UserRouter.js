f8f9391c77852788b419f8a467583a7c9bffd03a
Subham <subham@synergicsoftek.in>
2023-10-10 18:24:59 +0530

    Bug fix 10-10-2023

--- router/UserRouter.js
+++ router/UserRouter.js
@@ -3,7 +3,26 @@ const express = require("express"),
   bcrypt = require("bcrypt");
 
 const { db_Select, USER_TYPE_LIST } = require("../modules/MasterModule");
-const { getUserList, saveUser, getClientList, saveClientData } = require("../modules/UserModule");
+const {
+  getUserList,
+  saveUser,
+  getClientList,
+  saveClientData,
+} = require("../modules/UserModule");
+
+UserRouter.use((req, res, next) => {
+  console.log(req.url);
+  if (req.url != "/login") {
+    var user = req.session.user;
+    if (user) {
+      next();
+    } else {
+      res.redirect("/login");
+    }
+  } else {
+    next();
+  }
+});
 
 UserRouter.get("/login", (req, res) => {
   // var dt = new Date();
@@ -20,7 +39,8 @@ UserRouter.get("/login", (req, res) => {
 UserRouter.post("/login", async (req, res) => {
   var data = req.body;
   // dynamicNotify('fa fa-bell-o', 'Success', 'Test notification', 'success')
-  var select = "id, client_id, user_name, user_id, password, user_type, active_flag",
+  var select =
+      "id, client_id, user_name, user_id, password, user_type, active_flag",
     table_name = "md_user",
     whr = `user_id = '${data.email}' AND active_flag = 'Y'`,
     order = null;
@@ -54,19 +74,19 @@ UserRouter.get("/log_out", (req, res) => {
   res.redirect("/login");
 });
 
-UserRouter.get('/client', async (req, res) => {
-  var client_data = await getClientList()
+UserRouter.get("/client", async (req, res) => {
+  var client_data = await getClientList();
   var data = {
     client_data,
     header: "Client List",
   };
   res.render("client/view", data);
-})
+});
 
-UserRouter.get('/client_edit', async (req, res) => {
+UserRouter.get("/client_edit", async (req, res) => {
   var id = req.query.id,
-  client_data = [];
-  if(id > 0) client_data = await getClientList(id)
+    client_data = [];
+  if (id > 0) client_data = await getClientList(id);
   var data = {
     client_data,
     id,
@@ -76,19 +96,19 @@ UserRouter.get('/client_edit', async (req, res) => {
     header_url: "/client",
   };
   res.render("client/add", data);
-})
+});
 
-UserRouter.post('/client_save', async (req, res) => {
-  var data = req.body
-  var res_dt = await saveClientData(data, req.session.user.user_name)
+UserRouter.post("/client_save", async (req, res) => {
+  var data = req.body;
+  var res_dt = await saveClientData(data, req.session.user.user_name);
   req.session.message = {
     type: res_dt.suc > 0 ? "success" : "danger",
     message: res_dt.msg,
   };
-  res.redirect('/client')
-})
+  res.redirect("/client");
+});
 
-UserRouter.get('/manage_user', async (req, res) => {
+UserRouter.get("/manage_user", async (req, res) => {
   var user_data = await getUserList(0, req.session.user.client_id);
   // console.log(user_data);
   var data = {
@@ -97,12 +117,12 @@ UserRouter.get('/manage_user', async (req, res) => {
     header: "Manage User",
   };
   res.render("manage_user/view", data);
-})
+});
 
-UserRouter.get('/manage_user_edit', async (req, res) => {
+UserRouter.get("/manage_user_edit", async (req, res) => {
   var id = req.query.id,
-  user_data = [];
-  if(id > 0) user_data = await getUserList(id, req.session.user.client_id)
+    user_data = [];
+  if (id > 0) user_data = await getUserList(id, req.session.user.client_id);
   var data = {
     user_data,
     id,
@@ -112,24 +132,28 @@ UserRouter.get('/manage_user_edit', async (req, res) => {
     header_url: "/manage_user",
   };
   res.render("manage_user/add", data);
-})
+});
 
-UserRouter.post('/manage_user_save', async (req, res) => {
-  var data = req.body
-  var res_dt = await saveUser(data, req.session.user.user_name, req.session.user.client_id)
+UserRouter.post("/manage_user_save", async (req, res) => {
+  var data = req.body;
+  var res_dt = await saveUser(
+    data,
+    req.session.user.user_name,
+    req.session.user.client_id
+  );
   req.session.message = {
     type: res_dt.suc > 0 ? "success" : "danger",
     message: res_dt.msg,
   };
-  res.redirect('/manage_user')
-})
+  res.redirect("/manage_user");
+});
 
-UserRouter.post('/get_client_user_list_ajax', async (req, res) => {
+UserRouter.post("/get_client_user_list_ajax", async (req, res) => {
   var data = req.body,
-  client_id = req.session.user.client_id;
+    client_id = req.session.user.client_id;
   var res_dt = await getUserList(0, client_id, data.flag);
-  res_dt['user_type_list'] = USER_TYPE_LIST
-  res.send(res_dt)
-})
+  res_dt["user_type_list"] = USER_TYPE_LIST;
+  res.send(res_dt);
+});
 
 module.exports = { UserRouter };
