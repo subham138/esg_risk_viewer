2557648e796d3ea2f6f39dd28d8adf0f954a0d6d
Subham <subham@synergicsoftek.in>
2024-06-17 19:07:26 +0530

    increase parameterLimit in server 17.06.2024

--- server.js
+++ server.js
@@ -19,7 +19,13 @@ app.use(express.static(path.join(__dirname, "assets/")));
 // app.use(express.json()); //
 // app.use(express.urlencoded({ extended: false }));
 app.use(express.json({limit: '50mb'}));
-app.use(express.urlencoded({limit: '50mb', extended: false}));
+app.use(
+  express.urlencoded({
+    limit: "50mb",
+    extended: false,
+    parameterLimit: 1000000,
+  })
+);
 // END //
 
 // CONFIGURE SESSION //
