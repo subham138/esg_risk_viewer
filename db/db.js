a3eb3c3c306d48242782d4c1542600b1c5f6e4a9
Subham <subham@synergicsoftek.in>
2024-07-11 01:30:27 +0530

    db change

--- db/db.js
+++ db/db.js
@@ -1,23 +1,23 @@
 const mysql = require("mysql2");
 
 // LOCAL //
-const db = mysql.createPool({
-  connectionLimit: 10,
-  host: "localhost",
-  user: "root",
-  password: "",
-  // port: 3306,
-  database: "esg_risk_viewer",
-});
-
-// SERVER //
 // const db = mysql.createPool({
 //   connectionLimit: 10,
 //   host: "localhost",
 //   user: "root",
-//   password: "Cloud#1851",
+//   password: "",
+//   // port: 3306,
 //   database: "esg_risk_viewer",
 // });
+
+// SERVER //
+const db = mysql.createPool({
+  connectionLimit: 10,
+  host: "localhost",
+  user: "esg_risk_viewer",
+  password: "Cloud#1851",
+  database: "esg_risk_viewer",
+});
 
 db.getConnection((err, connection) => {
   if (err) console.log(err);
