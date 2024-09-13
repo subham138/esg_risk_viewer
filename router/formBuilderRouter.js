fe39eef4b0560c1f4f8794d7acf126b961cb34b0
Subham Samanta <subham@synergicsoftek.in>
2024-09-13 19:08:50 +0530

    Update on Calculator form builder

--- router/formBuilderRouter.js
+++ router/formBuilderRouter.js
@@ -2,6 +2,81 @@ const FBRouter = require('express').Router(),
 {db_Insert, INPUT_TYPE_LIST, SCOPE_LIST, db_Select, db_Delete} = require('../modules/MasterModule'),
 dateFormat = require("dateformat");
 
+FBRouter.get('/cal_sec_type', async (req, res) => {
+    var data = await db_Select('*', 'md_cal_sec_type', null, null)
+    var view_data = {
+        data,
+        header: 'Calculator Sector Type'
+    }
+    res.render('calculator/section_type/view', view_data)
+})
+
+FBRouter.get('/cal_sec_type_edit', async (req, res) => {
+    var dt = req.query
+    var id = dt.id,
+    type_list = [];
+    // console.log(dt);
+    var data = {suc:0,msg:[]}
+    if(id > 0){
+        data = await db_Select('*', 'md_cal_sec_type', `id=${id}`, null)
+        type_list = await getCalTypeList(0, data.suc > 0 ? data.msg[0].scope_id : 0)
+    }else{
+        type_list = await getCalTypeList(0, 0)
+    }
+    var view_data = {
+        data: data.suc > 0 ? data.msg : [],
+        type_list: type_list.suc > 0 ? type_list.msg : [],
+        header: "Calculator Type",
+        sub_header: "Add/Edit Calculator Sector Type",
+        header_url: `/cal_sec_type`,
+        id: id,
+    }
+    res.render('calculator/section_type/entry', view_data)
+})
+
+FBRouter.post('/cal_sec_type_edit', async (req, res) => {
+    var data = req.body,
+    user = req.session.user.user_name,
+    datetime = dateFormat(new Date(), 'yyyy-mm-dd HH:MM:ss');
+    console.log(data);
+    
+    var res_dt = await db_Insert('md_cal_sec_type', data.id > 0 ? `scope_id = ${data.scope_id}, type_id='${data.type_id}', sec_name = '${data.sec_name}', modified_by = '${user}', modified_dt = '${datetime}'` : '(scope_id, type_id, sec_name, created_by, created_dt)', `(${data.scope_id}, '${data.type_id}', '${data.sec_name}', '${user}', '${datetime}')`, data.id > 0 ? `id=${data.id}` : null, data.id > 0 ? 1 : 0)
+    if(res_dt.suc > 0){
+        req.session.message = {
+            type: "success",
+            message: "Saved successfully",
+        };
+        res.redirect(`/cal_sec_type`);
+    } else {
+        req.session.message = { type: "danger", message: "Data not saved" };
+        res.redirect(data.id > 0 ? `/cal_sec_type` : `/cal_sec_type`);
+    }
+})
+
+FBRouter.get('/cal_sec_type_del', async (req, res) => {
+    var id = req.query.id
+    if (id > 0) {
+        var res_dt = await db_Delete('md_cal_sec_type', `id = ${id}`);
+        req.session.message = { type: "danger", message: "Data deleted successfully" };
+    }else{
+        req.session.message = { type: "warning", message: "Data not deleted" };
+    }
+    res.redirect(`/cal_sec_type`);
+})
+
+FBRouter.post('/get_calc_type_list', async (req, res) => {
+    var data = req.body
+    var res_dt = await getCalTypeList(data.id > 0 ? data.id : 0, data.scope > 0 ? data.scope : 0)
+    res.send(res_dt)
+})
+
+const getCalTypeList = (id = 0, scope = 0) => {
+    return new Promise(async (resolve, reject) => {
+        var res_dt = await db_Select('id, type_name, type, scope', 'md_cal_type', id > 0 ? `id=${id}` : `${scope > 0 ? `scope = ${scope}` : ''}`, null)
+        resolve(res_dt)
+    })
+}
+
 FBRouter.get('/form_builder', async (req, res) => {
     var data = await db_Select('DISTINCT scope_id', 'md_cal_form_builder', null, 'ORDER BY scope_id asc')
     var scope_list = SCOPE_LIST
@@ -9,7 +84,8 @@ FBRouter.get('/form_builder', async (req, res) => {
 })
 
 FBRouter.get('/form_builder_edit', async (req, res) => {
-    var scope_dt = SCOPE_LIST, data = req.query, qr_dt = {}, q_header = '';
+    var scope_dt = SCOPE_LIST, data = req.query, qr_dt = {}, q_header = '',
+    sec_list = await db_Select('scope_id, type_id, sec_name', 'md_cal_sec_type', data.scope > 0 ? `scope_id = ${data.scope}` : null, null);
     if(data.scope > 0){
         var resDt = await db_Select('*', 'md_cal_form_builder', `scope_id=${data.scope}`, 'ORDER BY id asc')
         if(resDt.suc > 0){
@@ -28,7 +104,7 @@ FBRouter.get('/form_builder_edit', async (req, res) => {
     }
     console.log(qr_dt);
     
-    res.render('form_builder/entry', {scope: scope_dt, qr_dt, q_header, scope_id: data.scope})
+    res.render('form_builder/entry', {scope: scope_dt, qr_dt, q_header, scope_id: data.scope, sec_id: data.sec_id, sec_list: sec_list.suc > 0 ? sec_list.msg : []})
 })
 
 FBRouter.post('/form_builder_post', async (req, res) => {
