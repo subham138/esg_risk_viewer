6e1228971ca0d2c50121aa78e885c1381a693612
Subham Samanta <subham@synergicsoftek.in>
2024-10-04 16:40:44 +0530

    Accordian 04.10.2024

--- router/UserRouter.js
+++ router/UserRouter.js
@@ -216,12 +216,12 @@ UserRouter.post("/chk_user_login", async (req, res) => {
           var otp = Math.floor(1000 + Math.random() * 9000);
           console.log(otp);
           // req.session.message = {otp: otp}
-          var send_email = await sendOtp(data.email, chk_dt.msg[0].user_name, otp)
-          if(send_email.suc > 0){
+          // var send_email = await sendOtp(data.email, chk_dt.msg[0].user_name, otp)
+          // if(send_email.suc > 0){
           res_dt = { suc: 1, msg: chk_dt.msg[0], pin: otp };
-          }else{
-            res_dt = {suc: 0, msg: 'Email not send please try again after some time.'}
-          }
+          // }else{
+          //   res_dt = {suc: 0, msg: 'Email not send please try again after some time.'}
+          // }
         } else {
           res_dt = { suc: 1, msg: chk_dt.msg[0], pin: 0 };
         }
