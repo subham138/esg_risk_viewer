e38ac6d6538488fcadba35bb3441405e79b33b8f
Subham Samanta <subham@synergicsoftek.in>
2024-11-21 16:09:46 +0530

    Cal Dashboard 21.11.2024

--- modules/CalculatorModule.js
+++ modules/CalculatorModule.js
@@ -199,7 +199,7 @@ module.exports = {
     },
     getGhgCalList: (proj_id, client_id) => {
         return new Promise(async (resolve, reject) => {
-            var calSel = `a.id, a.client_id, a.scope, a.project_id, a.quest_id, a.sl_no, a.sec_id, a.act_id, a.emi_type_id, a.repo_period, a.repo_month, a.repo_mode_label, a.emi_type_unit_id, a.cal_val, a.emi_fact_val, a.co_val, c.act_name, d.emi_name`,
+            var calSel = `a.id, a.client_id, a.scope, a.project_id, a.quest_id, a.sl_no, a.sec_id, a.act_id, a.emi_type_id, a.repo_period, a.repo_month, a.repo_mode_label, a.emi_type_unit_id, a.cal_val, a.emi_fact_val, a.co_val, c.act_name, d.emi_name, b.sec_id cal_sec_id`,
                 calWhr = `a.quest_id=b.id AND a.act_id=c.id AND a.emi_type_id=d.id AND a.project_id = ${proj_id} AND a.client_id = ${client_id}`;
             var calVal = await db_Select(calSel, 'td_ghg_quest_cal a, md_cal_form_builder b, md_cal_act c, md_cal_emi_type d', calWhr, `ORDER BY a.scope, a.sl_no`)
             resolve(calVal)
